1b76af45e27b3307e0e0edf137cfccb14938a156
Jonathan B <greenbigfrog@gmail.com>
2019-08-14 21:16:01 +0200

    Initial version

---
+++ src/jobs/webhook.cr
@@ -0,0 +1,15 @@
+class TB::Worker::WebhookJob < Mosquito::QueuedJob
+  params embed : String, webhook_type : String
+
+  def perform
+    case webhook_type
+    when "admin"   then webhook = ENV["ADMIN_WEBHOOK"]
+    when "general" then webhook = ENV["GENERAL_WEBHOOK"]
+    else                raise "Invalid webhook type"
+    end
+
+    json = "{\"embeds\": [#{embed}]}"
+    res = HTTP::Client.post(webhook + "?wait=true", HTTP::Headers{"Content-Type" => "application/json"}, json)
+    raise "Error posting to webhook" unless res.success?
+  end
+end
---
+++ src/tb-worker.cr
@@ -0,0 +1,9 @@
+require "mosquito"
+require "discordcr"
+require "big"
+require "http/client"
+
+require "tb"
+require "./jobs/*"
+
+Mosquito::Runner.start
